b1a44cf10beb5864a26b8f9fd43f713a554a2011
Adão Biotto <adao.biotto@southsystem.com.br>
2026-07-23 09:34:17 -0300

    feat: adiciona tela de exercícios com acompanhamento

---
+++ exercicios.js
@@ -0,0 +1,16 @@
+function concluirExercicio(){
+
+const realizado =
+document.getElementById("realizado");
+
+if(realizado.checked){
+
+alert("Parabéns! Exercício concluído.");
+
+}else{
+
+alert("Marque a caixa quando terminar o exercício.");
+
+}
+
+}
