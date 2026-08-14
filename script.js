afa6827a0fb8e0ee572c2b57ee02f62524383e1a
Adão Biotto <adao.biotto@southsystem.com.br>
2026-08-14 11:40:27 -0300

    adicionando usuarios e tambem o botao para solicitar acesso para quem ainda n tem

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 function login() {
 
-    const usuario = document.getElementById("usuario").value.trim();
-    const senha = document.getElementById("senha").value;
+    const usuario =
+        document.getElementById("usuario").value.trim();
+
+    const senha =
+        document.getElementById("senha").value;
 
     if (
         (usuario.toLowerCase() === "lenine" && senha === "123") ||
-        (usuario.toLowerCase() === "dulce" && senha === "123")
+        (usuario.toLowerCase() === "dulce" && senha === "123") ||
+        (usuario.toLowerCase() === "vanessa" && senha === "123") ||
+        (usuario.toLowerCase() === "geral" && senha === "123")
     ) {
 
-        // Salva o nome do usuário
-        localStorage.setItem("usuarioLogado", usuario);
+        localStorage.setItem(
+            "usuarioLogado",
+            usuario
+        );
 
-        window.location.href = "exercicios.html";
+        window.location.href =
+            "exercicios.html";
 
     } else {
 
@@ -21,4 +29,18 @@ function login() {
 
 }
 
-localStorage.setItem("usuarioLogado", usuario);
+
+function solicitarAcesso() {
+
+    const mensagem =
+        "Olá! Gostaria de solicitar acesso ao aplicativo de exercícios físicos.";
+
+    const numeroWhatsApp =
+        "5551994431930";
+
+    const url =
+        `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensagem)}`;
+
+    window.open(url, "_blank");
+
+}
